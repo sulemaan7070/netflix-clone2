952f14b3f5946d74bd95e164a3a345eb8faa5a7f
sulemaan7070 <sulemaan7070@gmail.com>
2022-10-24 10:35:56 +0530

    logo asset changed to more reliable one

--- src/components-styles/Nav.js
+++ src/components-styles/Nav.js
@@ -33,9 +33,10 @@ function Nav() {
           whileTap={{ scale: 0.9 }}
           onClick={() => history("/")}
           className="nav__logo"
-          src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png"
+          src="https://i.imgur.com/jwmMGqt.png"
           alt=""
         />
+        
         {/*
          * The history.push(path) method is deprecated to "history(path"
          */}
